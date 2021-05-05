07f5d1acf4d34a3430e1fb89aeeeefe57d70f732
MRT0M <77304942+MRT0M@users.noreply.github.com>
2021-05-05 14:25:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,28 +17,28 @@ function setup() {
   hero=new Hero(100,175,250,200);
   fly=new Fly(hero.body,{x:100,y:100});
 
-  block1=new Block(350,150,50,50);
-  block2=new Block(350,200,50,50)
-  block3=new Block(350,250,50,50)
-  block4=new Block(350,300,50,50)
-  block5=new Block(450,150,50,50)
-  block6=new Block(450,200,50,50)
-  block7=new Block(450,250,50,50);
-  block8=new Block(450,300,50,50)
+  block1=new Block(350,200,50,50);
+  block2=new Block(350,250,50,50)
+  block3=new Block(350,300,50,50)
+  block4=new Block(350,350,50,50)
+  block5=new Block(450,200,50,50)
+  block6=new Block(450,250,50,50)
+  block7=new Block(450,300,50,50);
+  block8=new Block(450,350,50,50)
 
-  block9=new Block(550,150,50,50);
-  block10=new Block(550,200,50,50)
-  block11=new Block(550,250,50,50)
-  block12=new Block(550,300,50,50)
-  block13=new Block(650,150,50,50)
-  block14=new Block(650,200,50,50)
-  block15=new Block(650,250,50,50);
-  block16=new Block(650,300,50,50)
+  block9=new Block(550,200,50,50);
+  block10=new Block(550,250,50,50)
+  block11=new Block(550,300,50,50)
+  block12=new Block(550,350,50,50)
+  block13=new Block(650,200,50,50)
+  block14=new Block(650,250,50,50)
+  block15=new Block(650,300,50,50);
+  block16=new Block(650,350,50,50)
 
-  block17=new Block(350,50,50,50)
-  block18=new Block(450,50,50,50)
-  block19=new Block(550,50,50,50)
-  block20=new Block(650,50,50,50)
+  block17=new Block(350,150,50,50)
+  block18=new Block(450,150,50,50)
+  block19=new Block(550,150,50,50)
+  block20=new Block(650,150,50,50)
 
   monster = new Monster(800,275,200,200)
   Engine.run(engine)
